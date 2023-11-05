871b0f81896eede2870c887a83b75a639358dec7
Olivia Jarl <97847703+OliviaJarl@users.noreply.github.com>
2023-11-05 16:39:46 +0100

    added empty about page, added button

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "../components/Button";
+
+export default function About() {
+  return (
+    <>
+      this is the about page
+      <Button />
+    </>
+  );
+}
---
+++ app/components/Button.tsx
@@ -0,0 +1,3 @@
+export const Button = () => {
+  return <button>im a button</button>;
+};
